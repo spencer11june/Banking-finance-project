f0be34737d50469bdd85713aaa778da3592cb169
spencer11june <136250306+spencer11june@users.noreply.github.com>
2023-10-02 22:21:30 +0530

    Update main.tf

--- my-serverfiles/main.tf
+++ my-serverfiles/main.tf
@@ -1,12 +1,12 @@
 resource "aws_instance" "test-server" {
   ami           = "ami-0f5ee92e2d63afc18" 
   instance_type = "t2.micro" 
-  key_name = "BabucKeypair"
+  key_name = "Spencer4"
   vpc_security_group_ids= ["sg-00f7cae4b48423f8c"]
   connection {
     type     = "ssh"
     user     = "ubuntu"
-    private_key = file("./BabucKeypair.pem")
+    private_key = file(".Spencer4.pem")
     host     = self.public_ip
   }
   provisioner "remote-exec" {
